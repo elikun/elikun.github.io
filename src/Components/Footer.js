28d9afd3da66ee9962b068ca979a865dd16ad561
Eli <renneruonkoi@gmail.com>
2020-08-11 18:34:33 +0900

    Updated names.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
+              <li>Copyright &copy; 2020 Rennel Ongcoy</li>
+              <li>ReactJS Template by <a title="Tim Baker" href="http://www.timbakerdev.com/">Tim Baker</a></li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
